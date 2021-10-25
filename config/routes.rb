02e7b7ec09d5a9de25198ba4550f2d90e2e578a1
Rainey Lopes <rainey.lopes@gmail.com>
2021-10-25 11:19:35 -0300

    models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # A user can add a new bookmark (movie/list pair) to an existing list
+  # resources :lists, %i[index show new create] do # a ver Do we need a MoviesController?
+  #  resources :bookmarks, only: %i[index new create destroy]
+  end
+
+=begin
+Tati:
+root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
+Mário:
+  resources :movies, only: %i[index show] do
+
+    resources :lists do
+    resources :bookmarks, except: [:destroy]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
+Isabela:
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, except: [:destroy]
+  end
+  resources :bookmarks, only: [:destroy]
+end
+=end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require open-uri and json files
+#  url_original = 'https://api.themoviedb.org/3/movie/top_rated?api_key=<your_api_key>'
+#  url_le_wagon = url_original.sub! 'https://api.themoviedb.org/3', 'http://tmdb.lewagon.com'
+#  url_original = url_le_wagon.sub! '<your_api_key>', your_api
+#  poster = "#{prefix_poster}#{specif_movie}"
+#  fetch 'http://tmdb.lewagon.com/movie/top_rated'
+require 'json'
+require 'open-uri'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+puts 'Creating top movies...'
+
+uri = URI(url)
+response = Net::HTTP.get(uri)
+results = JSON.parse(response)
+window_width = "300"
+prefix_poster = "https://image.tmdb.org/t/p/#{window_width}"
+
+results['results'].each do |result|
+  poster_url = "#{prefix_poster}#{result['poster_path']}"
+  new_movie = Movie.new(title: result['title'], overview: result['overview'],
+                        poster_url: poster_url, rating: result['vote_average'])
+  new_movie.save!
+end
+puts 'Finished!...'
